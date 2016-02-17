38e03ffa2e2a5baccaf34c619a530c2c692fd722
Pete <peternatewood@gmail.com>
2016-02-17 11:19:47 -0500

    Add startGame function in controller

--- controller.js
+++ controller.js
@@ -2,3 +2,8 @@ var Controller = function(shape) {
   this.gameBoard = new tetrisBoard();
   this.gameView = new browserView({gameBoard: this.gameBoard});
 }
+Controller.prototype.startGame = function() {
+  this.gameBoard.blit();
+  this.gameView.animate();
+  this.gameBoard.cycleDropBlock();
+}
