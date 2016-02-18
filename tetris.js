6a1c843b8d620d09e38bc14ad396badc458a87ca
Pete <peternatewood@gmail.com>
2016-02-18 15:18:36 -0500

    Remove redundant objects in tetris.js

--- tetris.js
+++ tetris.js
@@ -7,9 +7,6 @@ var ready = function(fn) {
   }
 }
 ready(function() {
-  window.lineBlock = new Tetrinimo({element: 1, shape: TETRINIMO_SHAPES.line});
-  window.gameBoard = new TetrisBoard({tetrinimo: lineBlock});
-  window.gameView = new BrowserView({gameBoard: gameBoard});
   window.gameControl = new Controller();
 
   gameControl.startGame();
