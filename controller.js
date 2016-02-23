ca7f548f6bcf8ae4ec6a98acecbaa6feff552875
Pete <peternatewood@gmail.com>
2016-02-23 10:41:08 -0500

    Refactor random element and shape functions into controller

--- controller.js
+++ controller.js
@@ -1,3 +1,35 @@
+var generateRandomElements = function() {
+  var randElements = new Array;
+  var orderedElements = new Array;
+
+  for(var prop in CHEMICAL_ELEMENTS) {
+    if(CHEMICAL_ELEMENTS.hasOwnProperty(prop)) {
+      orderedElements.push(prop);
+    }
+  }
+
+  var length = new Number(orderedElements.length);
+
+  for(var i = 0; i < length; i++) {
+    var randIndex = Math.floor(Math.random() * orderedElements.length);
+    randElements.push(orderedElements[randIndex]);
+    orderedElements.splice(randIndex, 1);
+  }
+
+  return randElements;
+};
+var getRandomShape = function() {
+  var counter = 0;
+  var randNum = Math.floor(Math.random() * 7);
+
+  for(var prop in TETRINIMO_SHAPES) {
+    if(TETRINIMO_SHAPES.hasOwnProperty(prop)) {
+      if(counter == randNum) return TETRINIMO_SHAPES[prop];
+      counter++;
+    }
+  }
+};
+
 var Controller = function(shape) {
   this.gameBoard = new TetrisBoard();
   this.gameView = new BrowserView({
