8de0282948951d2f2ac052e3e888cc6c4ba8d687
Pete <peternatewood@gmail.com>
2016-03-05 14:49:42 -0500

    Update description for bohrium

--- element-constants.js
+++ element-constants.js
@@ -968,7 +968,7 @@ var CHEMICAL_ELEMENTS = {
     'background-color': '#EFEFEF',
     'border-color': '#818083',
     'color': 'black',
-    'descrip': "Named after the physicist, Niels Bohr."
+    'descrip': "Named after the Danish physicist, Niels Bohr. A synthetic element, too little has been produced to have any practical uses."
   },
 
   108: {
