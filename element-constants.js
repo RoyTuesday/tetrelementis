f998bf114675f60181462d63323d92ec8116dcb7
Pete <peternatewood@gmail.com>
2016-03-05 14:41:04 -0500

    Update description for einsteinium

--- element-constants.js
+++ element-constants.js
@@ -896,7 +896,7 @@ var CHEMICAL_ELEMENTS = {
     'background-color': '#EFEFEF',
     'border-color': '#818083',
     'color': 'black',
-    'descrip': "Named after the physicist, Albert Einstein."
+    'descrip': "Named after the physicist, Albert Einstein. A synthetic element, too little has been produced to have any practical uses."
   },
 
   100: {
