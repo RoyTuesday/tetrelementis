afae1967acf2caac9a2db7a036e1ec42635a8687
Pete <peternatewood@gmail.com>
2017-02-18 11:02:28 -0500

    Add function to play notes

--- audio.js
+++ audio.js
@@ -7,7 +7,7 @@ catch (err) {
 }
 
 function playRotateSound(audio) {
-  if (gAudio === false) return;
+  if (audio === false) return;
   var osc = audio.createOscillator();
   var gain = audio.createGain();
   var start = audio.currentTime;
@@ -26,7 +26,7 @@ function playRotateSound(audio) {
 }
 
 function playSlideSound(audio) {
-  if (gAudio === false) return;
+  if (audio === false) return;
   var osc = audio.createOscillator();
   var gain = audio.createGain();
   var start = audio.currentTime;
@@ -45,7 +45,7 @@ function playSlideSound(audio) {
 }
 
 function playDropSound(audio) {
-  if (gAudio === false) return;
+  if (audio === false) return;
   var osc = audio.createOscillator();
   var gain = audio.createGain();
   var start = audio.currentTime;
@@ -60,4 +60,35 @@ function playDropSound(audio) {
   gain.gain.linearRampToValueAtTime(0, start);
   gain.gain.linearRampToValueAtTime(0.1, start + 0.01);
   gain.gain.linearRampToValueAtTime(0, start + 0.1);
+}
+
+function playNote(audio, gain, note, start, end) {
+  var osc = audio.createOscillator();
+  var freq;
+  switch (note[0]) {
+    case 'c'  : freq = 16.35; break;
+    case 'db' : freq = 17.32; break;
+    case 'd'  : freq = 18.35; break;
+    case 'eb' : freq = 19.45; break;
+    case 'e'  : freq = 20.60; break;
+    case 'f'  : freq = 21.83; break;
+    case 'gb' : freq = 23.12; break;
+    case 'g'  : freq = 24.50; break;
+    case 'ab' : freq = 25.96; break;
+    case 'a'  : freq = 27.50; break;
+    case 'bb' : freq = 29.14; break;
+    case 'b'  : freq = 30.87; break;
+  }
+
+  osc.type = 'triangle';
+  osc.frequency.value = Math.pow(2, note[1]) * freq;
+  osc.connect(gain);
+  gain.connect(audio.destination);
+
+  osc.start(start);
+  osc.stop(end);
+
+  gain.gain.linearRampToValueAtTime(0, start);
+  gain.gain.linearRampToValueAtTime(0.1, start + 0.01);
+  gain.gain.linearRampToValueAtTime(0, end);
 }
